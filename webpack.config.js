ed7b4b2c7fbba5926a1cdf21c479a53bc7490a62
Espen Klem <espen.klem@gmail.com>
2019-07-05 14:40:11 +0200

    tape and tape-run now working, woop-woop!'

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const package = require('./package.json')
+const glob = require('glob')
+
+
+  // Generating browser version of stopword
+
+  module.exports =  {
+    mode: 'production',
+    entry: './headline-parser.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'headline-parser.' + package.version+ '.js',
+      library: 'headlineP'
+    },
+    devtool: "none", // prevent webpack from using eval() on my module
+  }
+
+  // Generating test script for the browser
+  
+module.exports = {
+    mode: 'production',
+    entry: glob.sync('./test/test.js'),
+    output: {
+      path: path.resolve(__dirname, './test/sandbox'),
+      filename: 'bundle.js'
+    },
+    node: {
+      fs: 'empty'
+    }
+}
