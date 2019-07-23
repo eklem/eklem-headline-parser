f37d3cc4279e6494c29d10376a2607fa996b9486
Espen Klem <espen.klem@gmail.com>
2019-07-23 21:50:48 +0200

    Removing duplicates and sorting on most used

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 const findKeywords = function( headline, body, cutoff = 0){
   console.log('headline: ' + headline)
+  // remove duplicates in headline
+  headline = [...new Set(headline)]
+  console.log('headline: ' + headline)
   console.log('body:     ' + body)
   console.log('cutoff:   ' + cutoff)
 
   // keywords array of word objects with word count
-  let keywordsAccumulated = []
+  let keywordsCount = []
 
   // returned array (cutoff if != 0)
   let keywords = []
-
 
   // loop through headline
   for (var i = 0; i < headline.length; i++) {
     // loop through body with each of the words in headline
     for (var j = 0; j < body.length; j++) {
-      // headline/body mathching function
+      // headline/body mathching
       if (headline[i] === body[j]) {
-        // check if word exists in keywordsAccumulated first
-        let existing = findExistingWord(keywordsAccumulated, 'word', headline[i])
+        // check if word exists in keywordsCount first
+        let existing = findExistingWord(keywordsCount, 'word', headline[i])
         if (existing > -1) {
           console.log('Should update count on word: ' + headline[i] + ' indexOf ' + existing)
-          keywordsAccumulated[existing].count++
+          keywordsCount[existing].count++
           // update count on word object
         } else {
           // create object and push to array
           let wordObj = {word: headline[i], count: 1}
-          keywordsAccumulated.push(wordObj)
+          keywordsCount.push(wordObj)
         }
-        console.dir(keywordsAccumulated)
+        console.dir(keywordsCount)
       }
     }
-    // check if word is not already looped through, store in headlineParsed
   }
-  
-
-
+  // Sort on most times used in body
+  keywordsCount.sort(compare)
+  console.dir(keywordsCount)
 }
 
 function findExistingWord(array, attr, value) {
@@ -46,7 +47,17 @@ function findExistingWord(array, attr, value) {
   return -1;
 }
 
-const titleArray = ['I', 'have', 'some', 'interesting', 'words']
+function compare( a, b ) {
+  if ( a.count > b.count ){
+    return -1;
+  }
+  if ( a.count < b.count ){
+    return 1;
+  }
+  return 0;
+}
+
+const titleArray = ['words', 'I', 'have', 'some', 'interesting', 'words', 'words']
 const bodyArray = ['There', 'is', 'a', 'chance', 'that', 'some', 'words', 'are', 'interesting', 'words', 'interesting', 'words']
 
 findKeywords(titleArray,bodyArray)
