f26eb6476d4fff4f88bd17fab24852862b425ac3
Espen Klem <espen.klem@gmail.com>
2019-07-24 15:47:48 +0200

    Tests okay Need one other language w/ stopwords

--- test/test.js
+++ test/test.js
@@ -1,26 +1,31 @@
-const headlineParser = require('../index.js')
+const ehp = require('../index.js')
+const sw = require ('stopword')
 const test = require('tape')
-const findKeywords = headlineParser.findKeywords
+const findKeywords = ehp.findKeywords
 
 //const titleArray = ['her', 'var', 'det', 'bra']
-const titleArray = ['words', 'I', 'have', 'some', 'interesting', 'words', 'words']
-const bodyArray = ['There', 'is', 'a', 'chance', 'that', 'some', 'words', 'are', 'interesting', 'words', 'interesting', 'words']
+const titleArray = [
+  ['words', 'I', 'have', 'some', 'interesting', 'words', 'words'],
+  ['I', 'you', 'some', 'it', 'the']
+]
+const bodyArray = [
+  ['There', 'is', 'a', 'chance', 'that', 'some', 'words', 'are', 'interesting', 'words', 'interesting', 'words', 'I', 'think'],
+  ['I', 'think', 'there', 'is', 'a', 'chance', 'that', 'this', 'text', 'will', 'return', 'some', 'keywords']
+]
 
 
-
-
-
-
-
-
-
-
-
-
-
-test('return top two keywords sorted by relevance', function (t) {
-  t.plan(1)
-  t.deepEqual(findKeywords(titleArray,bodyArray,2), ['words','interesting'])
+test('return top keywords sorted by relevance, first two, then all (three)', function (t) {
+  t.plan(2)
+  t.deepEqual(findKeywords(titleArray[0],bodyArray[0],2), ['words','interesting'])
+  t.deepEqual(findKeywords(titleArray[0],bodyArray[0]), ['words','interesting','I','some'])
 })
 
+test('same as above. Returns two when stopwords are removed from headline first', function (t) {
+  t.plan(1)
+  t.deepEqual(findKeywords(sw.removeStopwords(titleArray[0]),bodyArray[0]), ['words','interesting'])
+})
 
+test('Only stopwords in headline, they are removed, should return no keywords', function (t) {
+  t.plan(1)
+  t.deepEqual(findKeywords(sw.removeStopwords(titleArray[1]),bodyArray[1]), [])
+})
