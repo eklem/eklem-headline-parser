102a0d43d1414b2a153a0d27d7142cf3bc35cc82
Espen Klem <espen.klem@gmail.com>
2020-12-18 21:42:42 +0100

    Browser testing w/ webpack v5 Also * .js.map file added * Removing old naming styled files (w/ version numbers)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const pkg = require('./package.json')
 const glob = require('glob')
+const webpack = require('webpack')
 
 module.exports = [
   // Generating browser version of stopword
@@ -9,22 +9,10 @@ module.exports = [
     entry: './index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'headline-parser.' + pkg.version + '.js',
+      filename: 'headline-parser.js',
       library: 'ehp'
     },
-    devtool: 'none' // prevent webpack from using eval() on my module
-  },
-
-  // Generating "latest" browser version of stopword (same as above)
-  {
-    mode: 'production',
-    entry: './index.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'headline-parser.latest.js',
-      library: 'ehp'
-    },
-    devtool: 'none' // prevent webpack from using eval() on my module
+    devtool: 'hidden-source-map' // prevent webpack from using eval() on my module
   },
 
   // Generating test script for the browser
@@ -35,8 +23,24 @@ module.exports = [
       path: path.resolve(__dirname, './test/sandbox'),
       filename: 'bundle.js'
     },
+    resolve: {
+      fallback: {
+        fs: false,
+        path: require.resolve('path-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/')
+      }
+    },
     node: {
-      fs: 'empty'
-    }
+      global: true,
+      __filename: false,
+      __dirname: false
+    },
+    plugins: [
+      // fix "process is not defined" error:
+      new webpack.ProvidePlugin({
+        process: 'process/browser'
+      })
+    ]
   }
 ]
