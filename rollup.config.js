edc01de98131675d10cfad84719fc9432c1f853e
Espen Klem <espen.klem@gmail.com>
2022-09-06 20:19:37 +0200

    Minified versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,34 @@
-// Something like this:
-
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
-import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
 
 export default [
   // browser-friendly UMD build
+  // CommonJS (for Node) and ES module (for bundlers) build.
   {
     input: './src/index.js',
     output: [
-      { name: 'ehp', file: pkg.browser, format: 'umd' }
+      { name: 'ehp', file: './dist/eklem-headline-parser.umd.js', format: 'umd', exports: 'named' },
+      { file: './dist/eklem-headline-parser.cjs.js', format: 'cjs' },
+      { file: './dist/eklem-headline-parser.esm.mjs', format: 'es' }
     ],
     plugins: [
       resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-      json() // for Rollup to be able to read content from package.json
+      commonjs() // so Rollup can convert `ms` to an ES module
     ]
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
+  // Minified versions
   {
     input: './src/index.js',
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { name: 'ehp', file: './dist/eklem-headline-parser.umd.min.js', format: 'umd', exports: 'named' },
+      { file: './dist/eklem-headline-parser.cjs.min.js', format: 'cjs' },
+      { file: './dist/eklem-headline-parser.esm.min.mjs', format: 'es' }
     ],
     plugins: [
       resolve(), // so Rollup can find `ms`
       commonjs(), // so Rollup can convert `ms` to an ES module
-      json()
+      terser() // Minify
     ]
   }
 ]
